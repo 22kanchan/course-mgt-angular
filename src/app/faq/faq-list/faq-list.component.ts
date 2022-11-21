7d4b37ecbc15cf81560a8505ee8423b71acafc2e
22kanchan <kanchankaushal22@gmail.com>
2022-11-21 15:43:35 +0530

    crud of faq

--- src/app/faq/faq-list/faq-list.component.ts
+++ src/app/faq/faq-list/faq-list.component.ts
@@ -1,58 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { animate, state, style, transition, trigger} from '@angular/animations';
-import { Faq } from "src/app/_models/Faq";
 import { FaqService } from 'src/app/_services/faq.service';
-import { first } from 'rxjs/operators';
-
+import { Faq } from 'src/app/_models/Faq';
 
 declare var window: any;
+
 @Component({
   selector: 'app-faq-list',
   templateUrl: './faq-list.component.html',
-  styleUrls: ['./faq-list.component.scss'],
-  animations:[
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
+  styleUrls: ['./faq-list.component.scss']
 })
-export class FaqListComponent {
-  allFaq: Faq[]=[];
-  idToDelete: number = 0;
-  deleteModal: any;
-  faq: any;
-  constructor(private faqService:FaqService){}
+export class FaqListComponent implements OnInit {
+allFaq: Faq[] = [];
+deleteModal: any;
+idTodelete:number=0;
+  constructor(private faqService: FaqService) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.deleteModal = new window.bootstrap.Modal(
       document.getElementById('deleteModal')
-    );
+    ) ;
     this.getAll();
   }
 
-
   getAll(){
-    this.faqService.getAll().subscribe((data)=>{
-      this.allFaq = data ;
+    this.faqService.getAll().subscribe((data: Faq[]) => {
+      this.allFaq = data;
     });
   }
 
-
   openDeleteModal(id:number){
-    this.idToDelete = id;
+    this.idTodelete = id;
     this.deleteModal.show();
   }
 
+  delete(){
+    this.faqService.delete(this.idTodelete).subscribe({
+      next: (data) => {
+this.allFaq = this.allFaq.filter(_=>_.id != this.idTodelete)
+this.deleteModal.hide();
+      },
+    });
+  }
 
- delete(){
-  this.faqService.delete(this.idToDelete).subscribe({
-    next: (data) => {
-      this.allFaq = this.allFaq.filter(_=>_.id != this.idToDelete)
-      this.deleteModal.hide();
-    }
-  })
- }
 }
-
